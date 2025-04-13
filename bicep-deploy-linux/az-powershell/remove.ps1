6b4f93d531ea776fc18983f0ebd138971a5ed80c
Espenk <espenkleivane@gmail.com>
2025-04-13 21:59:34 +0200

    bicep

--- bicep-deploy-linux/az-powershell/remove.ps1
+++
@@ -1,3 +0,0 @@
-$resourceGroup = "Rg-iac-deploy-linux-fun-12"
-
-Get-AzResourceGroup -Name $resourceGroup | Remove-AzResourceGroup -Force -AsJob
--- bicep-deploy-linux/powershell_/deploy.ps1
+++ bicep-deploy-linux/powershell_/deploy.ps1
@@ -1,16 +1,19 @@
 # Function to log information to a file
-function Log-Information {
-    param (
+# Function to append to the log file
+function Write-Log {
+    param(
         [string]$Message
     )
-    $logFile = "log.txt"
-    Add-Content -Path $logFile -Value $Message
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logEntry = "$timestamp - $Message"
+    $logFile = "../log.txt"
+    Add-Content -Path $logFile -Value $logEntry
 }
 
 # Get the current date and log it
 $now = Get-Date
 Write-Output $now
-Log-Information -Message $now
+Write-Log -Message $now
 
 # Generate a simple VM name with a random number
 $simpleVmName = "Vm-test123it" # "Vm-$(Get-Random)"
@@ -32,9 +35,9 @@ Write-Output $adminU
 Write-Output $simpleVmName
 
 # Log resource group and admin information
-Log-Information -Message $resourceGroup
-Log-Information -Message $adminU
-Log-Information -Message $simpleVmName
+Write-Log -Message $resourceGroup
+Write-Log -Message $adminU
+Write-Log -Message $simpleVmName
 
 # Reference: https://github.com/Azure/azure-cli/issues/25710
 # Disable using binary from path for Bicep
@@ -46,10 +49,10 @@ New-AzResourceGroup -Name $resourceGroup -Location $location -Tag $tags -Force
 
 # Deploy all resources in the resource group with no data disk to an existing VNet
 
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile ../main_deploy_vm_no_extra_disk_to_outside_vnet.bicep `
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile "../templates/main_deploy_vm_no_extra_disk_to_outside_vnet.bicep" `
     -vmName $simpleVmName `
     -adminUsername $adminU `
     -resourceGroupVnetName $rgVnetName `
     -virtualNetworkName $vnet `
     -subnetName $subnet `
-    # -WhatIf
+    -WhatIf
